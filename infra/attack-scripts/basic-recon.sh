6100cbac842d7ec0e2f0a3bd18d96c7250f88996
Incogn1toBro <griffen-0@hotmail.com>
2026-03-15 13:33:05 +1100

    Create basic-recon.sh

---
+++ infra/attack-scripts/basic-recon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TARGET="INSERT DVWA IP"
+
+echo "=== 1. Nmap Port Scan ==="
+nmap -sS -p 80 $TARGET
+
+echo "=== 2. Web Fingerprint ==="
+curl -s -A "Mozilla/5.0 (Nmap NSE)" http://$TARGET:8080/ | head -10
+
+echo "=== 3. DVWA Directory Check ==="
+curl -s http://$TARGET:8080/dvwa/setup.php | grep -i dvwa
