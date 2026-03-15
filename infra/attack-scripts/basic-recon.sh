0a8a3626321fc648e0b3320006d37cece199e6e6
Incogn1toBro <griffen-0@hotmail.com>
2026-03-15 14:52:15 +1100

    Update basic-recon.sh

--- infra/attack-scripts/basic-recon.sh
+++ infra/attack-scripts/basic-recon.sh
@@ -5,7 +5,7 @@ echo "=== 1. Nmap Port Scan ==="
 nmap -sS -p 80 $TARGET
 
 echo "=== 2. Web Fingerprint ==="
-curl -s -A "Mozilla/5.0 (Nmap NSE)" http://$TARGET:8080/ | head -10
+curl -v -L -A "Mozilla/5.0 (Nmap NSE)" http://$TARGET:8080/ | head -20
 
 echo "=== 3. DVWA Directory Check ==="
 curl -s http://$TARGET:8080/setup.php | grep -i dvwa
